ba68b96d4f2c06bd4e68c0a1b91147ec33d63532
Cao Cự Chính <chinhcaocu@gmail.com>
2024-12-11 22:17:28 +0700

    Added tabs and extra validation to form

--- src/app/recruit/job-description/[id]/apply/_components/ApplyForm.tsx
+++ src/app/recruit/job-description/[id]/apply/_components/ApplyForm.tsx
@@ -13,11 +13,18 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Card } from "@/components/ui/card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 
-const ApplyForm = ({ data, question_title }: { data: string[], question_title:string[] }) => {
+const ApplyForm = ({
+  data,
+  question_title,
+}: {
+  data: string[];
+  question_title: string[];
+}) => {
   const dynamicSchema = z.object(
     data.reduce((acc, item) => {
       acc[item] = z
@@ -95,141 +102,203 @@ const ApplyForm = ({ data, question_title }: { data: string[], question_title:st
   const [studentID, setStudentID] = useState("VS");
   const [schoolEmail, setSchoolEmail] = useState("@stu.vinschool.edu.vn");
   const [manual, setManual] = useState(false);
+  const { formState } = form;
+  const [activeTab, setActiveTab] = useState("personal-info");
+
+  useEffect(() => {
+    const hasPersonalInfoErrors = Object.keys(formState.errors).some((key) =>
+      [
+        "name",
+        "student_id",
+        "class",
+        "school_email",
+        "private_email",
+        "facebook",
+      ].includes(key)
+    );
+
+    const hasQuestionErrors = Object.keys(formState.errors).some((key) =>
+      data.includes(key)
+    );
+
+    if (hasPersonalInfoErrors) {
+      setActiveTab("personal-info");
+    } else if (hasQuestionErrors && activeTab !== "questions") {
+      setActiveTab(activeTab);
+    }
+  }, [activeTab, data, formState.errors]);
+
   return (
-    <Card className="px-4 sm:px-12 py-8 mt-8 w-full lg:w-[70%]">
-      <h1 className="font-bold text-2xl mb-1">Thông tin cá nhân</h1>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Họ và tên</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="student_id"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Mã số học sinh</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    onChange={(e: any) => {
-                      field.onChange(e);
-                      if (!manual) {
-                        setSchoolEmail(
-                          (e.target.value.match(/\d+/)
-                            ? e.target.value.match(/\d+/)
-                            : "") + "@stu.vinschool.edu.vn"
-                        );
-                      }
-                      setStudentID(e.target.value);
-                    }}
-                    value={studentID}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="class"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Lớp</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="school_email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email trường</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    value={schoolEmail}
-                    onChange={(e: any) => {
-                      field.onChange(e);
-                      if (!manual) {
-                        setManual(!manual);
-                      }
-                      setSchoolEmail(e.target.value);
-                    }}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="private_email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email riêng {`(để làm việc)`}</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="facebook"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>
-                  Profile <span className="text-[#0966ff]">Facebook</span>
-                </FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <h1 className="font-bold text-2xl text-primary !mb-[-25px]">
-            Câu hỏi chuyên môn
-          </h1>
-          {data.map((question, index) => (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 w-full flex items-center justify-center"
+      >
+        <Tabs
+          defaultValue="personal-info"
+          value={activeTab}
+          onValueChange={setActiveTab}
+          className="px-4 sm:px-12 py-8 mt-8 w-full sm:w-[80%]"
+        >
+          <TabsList className="flex items-center h-[max-content] justify-center flex-wrap flex-row p-2 mb-8">
+            <TabsTrigger value="personal-info" className="flex-1 py-2">
+              Thông tin cá nhân
+            </TabsTrigger>
+            <TabsTrigger value="questions" className="flex-1 py-2">
+              Câu hỏi chuyên môn
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="personal-info" className="flex flex-col gap-8">
+            <h1 className="font-bold text-2xl">Thông tin cá nhân</h1>
             <FormField
-              key={question}
               control={form.control}
-              name={question as keyof FormData}
+              name="name"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="text-primary">{question_title[index]}</FormLabel>
+                  <FormLabel>Họ và tên</FormLabel>
                   <FormControl>
-                    <Textarea {...field} className="border border-primary" />
+                    <Input {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
-          ))}
-
-          <Button type="submit" className="w-full">
-            Submit
-          </Button>
-        </form>
-      </Form>
-    </Card>
+            <FormField
+              control={form.control}
+              name="student_id"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Mã số học sinh</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      onChange={(e: any) => {
+                        field.onChange(e);
+                        if (!manual) {
+                          setSchoolEmail(
+                            (e.target.value.match(/\d+/)
+                              ? e.target.value.match(/\d+/)
+                              : "") + "@stu.vinschool.edu.vn"
+                          );
+                        }
+                        setStudentID(e.target.value);
+                      }}
+                      value={studentID}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="class"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Lớp</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="school_email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email trường</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      value={schoolEmail}
+                      onChange={(e: any) => {
+                        field.onChange(e);
+                        if (!manual) {
+                          setManual(!manual);
+                        }
+                        setSchoolEmail(e.target.value);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="private_email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email riêng {`(để làm việc)`}</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="facebook"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Profile <span className="text-[#0966ff]">Facebook</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <TabsList className="w-full">
+              <TabsTrigger
+                value="questions"
+                className="flex flex-row gap-2 bg-[#f7c325] text-white w-full py-2 hover:opacity-90"
+              >
+                Câu hỏi chuyên môn <FaLongArrowAltRight />
+              </TabsTrigger>
+            </TabsList>
+          </TabsContent>
+          <TabsContent value="questions" className="flex flex-col gap-8">
+            <h1 className="font-bold text-2xl text-primary ">
+              Câu hỏi chuyên môn
+            </h1>
+            {data.map((question, index) => (
+              <FormField
+                key={question}
+                control={form.control}
+                name={question as keyof FormData}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{question_title[index]}</FormLabel>
+                    <FormControl>
+                      <Textarea {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ))}
+            <TabsList className="w-full">
+              <TabsTrigger
+                value="personal-info"
+                className="flex flex-row gap-2 bg-[#f7c325] text-white w-full py-2 hover:opacity-90"
+              >
+                <FaLongArrowAltLeft /> Thông tin cá nhân
+              </TabsTrigger>
+            </TabsList>
+            <Button type="submit" className="w-full">
+              Submit
+            </Button>
+          </TabsContent>
+        </Tabs>
+      </form>
+    </Form>
   );
 };
 
